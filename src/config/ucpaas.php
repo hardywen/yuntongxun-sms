f3f065372227a5b058df8f5228d588f641eb3014
hardywen <375480616@qq.com>
2015-09-29 17:15:25 +0800

    add file

---
+++ src/config/ucpaas.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    //主账户id
+    'sid' => '',
+
+    //应用Id
+    'appId' => '',
+
+    //账户授权令牌
+    'token' => '',
+
+
+    //Rest api 地址
+    'restUrl' => 'https://app.cloopen.com:8883',
+
+    //Rest api 版本
+    'softVersion' => '2013-12-26',
+
+];
